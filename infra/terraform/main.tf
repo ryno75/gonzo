f1a7f09c67e66498514eaa62d4f09c97b4e7253f
Ryan Kennedy <rkennedy@2ndwatch.com>
2018-06-05 16:38:03 -0700

    reorganizing main.tf, adding webhooks for testNodeJS

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -6,6 +6,9 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
+#
+# CodeBuild Resources
+#
 resource "aws_iam_role" "codebuild" {
   name               = "codebuild_service"
   path               = "/gonzo/"
@@ -16,34 +19,6 @@ resource "aws_iam_role_policy" "codebuild" {
   name   = "codebuild_service_policy"
   role   = "${aws_iam_role.codebuild.id}"
   policy = "${data.aws_iam_policy_document.codebuild_access.json}"
-}
-
-resource "aws_iam_role" "codedeploy" {
-  name               = "codedeploy_service"
-  path               = "/gonzo/"
-  assume_role_policy = "${data.aws_iam_policy_document.codedeploy_arpdoc.json}"
-}
-
-resource "aws_iam_role_policy" "codedeploy" {
-  name   = "codedeploy_service_policy"
-  role   = "${aws_iam_role.codedeploy.id}"
-  policy = "${data.aws_iam_policy_document.codedeploy_access.json}"
-}
-
-resource "aws_iam_role" "codepipeline" {
-  name               = "codepipeline_service"
-  path               = "/gonzo/"
-  assume_role_policy = "${data.aws_iam_policy_document.codepipeline_arpdoc.json}"
-}
-
-resource "aws_iam_role_policy" "codepipeline" {
-  name   = "codepipeline_service_policy"
-  role   = "${aws_iam_role.codepipeline.id}"
-  policy = "${data.aws_iam_policy_document.codepipeline_access.json}"
-}
-
-resource "aws_s3_bucket" "code_artifacts" {
-  bucket = "${local.artifact_bucket}"
 }
 
 resource "aws_codebuild_project" "gonzo_test" {
@@ -73,6 +48,21 @@ resource "aws_codebuild_project" "gonzo_test" {
   source {
     type = "CODEPIPELINE"
   }
+}
+
+#
+# CodeDeploy Resources
+#
+resource "aws_iam_role" "codedeploy" {
+  name               = "codedeploy_service"
+  path               = "/gonzo/"
+  assume_role_policy = "${data.aws_iam_policy_document.codedeploy_arpdoc.json}"
+}
+
+resource "aws_iam_role_policy" "codedeploy" {
+  name   = "codedeploy_service_policy"
+  role   = "${aws_iam_role.codedeploy.id}"
+  policy = "${data.aws_iam_policy_document.codedeploy_access.json}"
 }
 
 resource "aws_codedeploy_app" "gonzo_test" {
@@ -112,13 +102,43 @@ resource "aws_codedeploy_deployment_group" "gonzo_test" {
   }
 }
 
-data "template_file" "put_webhook_json" {
+#
+# CodePipeline Resources
+#
+resource "aws_s3_bucket" "code_artifacts" {
+  bucket = "${local.artifact_bucket}"
+}
+
+resource "aws_iam_role" "codepipeline" {
+  name               = "codepipeline_service"
+  path               = "/gonzo/"
+  assume_role_policy = "${data.aws_iam_policy_document.codepipeline_arpdoc.json}"
+}
+
+resource "aws_iam_role_policy" "codepipeline" {
+  name   = "codepipeline_service_policy"
+  role   = "${aws_iam_role.codepipeline.id}"
+  policy = "${data.aws_iam_policy_document.codepipeline_access.json}"
+}
+
+data "template_file" "gonzo_webhook_json" {
   template = "${file("data/webhook.json.tpl")}"
 
   vars {
     webhook_name  = "${local.webhook_name}"
     pipeline_name = "${local.pipeline_name}"
-    target_action = "Source"
+    target_action = "${local.src_action_name}"
+    token         = "${var.github_token}"
+  }
+}
+
+data "template_file" "testNodeJS_webhook_json" {
+  template = "${file("data/webhook.json.tpl")}"
+
+  vars {
+    webhook_name  = "${local.tnjs_webhook_name}"
+    pipeline_name = "${local.pipeline_name}"
+    target_action = "${local.tnjs_src_action_name}"
     token         = "${var.github_token}"
   }
 }
@@ -136,17 +156,33 @@ resource "aws_codepipeline" "test_multi_source" {
     name = "Source"
 
     action {
-      name             = "Source"
+      name             = "${local.src_action_name}"
       category         = "Source"
       owner            = "ThirdParty"
       provider         = "GitHub"
       output_artifacts = ["gonzo-src"]
       version          = "1"
-      run_order        = "1"
 
       configuration {
         Owner                = "${local.github_owner}"
         Repo                 = "${local.repo_name}"
+        Branch               = "master"
+        OAuthToken           = "${var.github_token}"
+        PollForSourceChanges = "false"
+      }
+    }
+
+    action {
+      name             = "${local.tnjs_src_action_name}"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      output_artifacts = ["testNodeJS-src"]
+      version          = "1"
+
+      configuration {
+        Owner                = "2ndWatch"
+        Repo                 = "${local.tnjs_repo_name}"
         Branch               = "master"
         OAuthToken           = "${var.github_token}"
         PollForSourceChanges = "false"
@@ -158,14 +194,13 @@ resource "aws_codepipeline" "test_multi_source" {
     name = "Build"
 
     action {
-      name             = "Build"
+      name             = "gonzo"
       category         = "Build"
       owner            = "AWS"
       provider         = "CodeBuild"
       input_artifacts  = ["gonzo-src"]
       output_artifacts = ["gonzo-bld"]
       version          = "1"
-      run_order        = "2"
 
       configuration {
         ProjectName = "${local.codebuild_proj_name}"
@@ -177,17 +212,30 @@ resource "aws_codepipeline" "test_multi_source" {
     name = "Deploy"
 
     action {
-      name            = "Deploy"
+      name            = "gonzo"
       category        = "Deploy"
       owner           = "AWS"
       provider        = "CodeDeploy"
       input_artifacts = ["gonzo-bld"]
       version         = "1"
-      run_order       = "3"
 
       configuration {
         ApplicationName     = "${aws_codedeploy_deployment_group.gonzo_test.app_name}"
         DeploymentGroupName = "${aws_codedeploy_deployment_group.gonzo_test.deployment_group_name}"
+      }
+    }
+
+    action {
+      name            = "testNodeJS"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "ElasticBeanstalk"
+      input_artifacts = ["testNodeJS-src"]
+      version         = "1"
+
+      configuration {
+        ApplicationName = "testNodeJS"
+        EnvironmentName = "Testnodejs-env"
       }
     }
   }
@@ -196,7 +244,7 @@ resource "aws_codepipeline" "test_multi_source" {
   # The following provisioners require the AWS CLI to be installed
   # and configured with the same profile being used for Terraform
   provisioner "local-exec" {
-    command = "aws codepipeline put-webhook --cli-input-json '${data.template_file.put_webhook_json.rendered}'"
+    command = "aws codepipeline put-webhook --cli-input-json '${data.template_file.gonzo_webhook_json.rendered}'"
 
     environment {
       AWS_DEFAULT_PROFILE = "${var.aws_profile}"
@@ -206,6 +254,24 @@ resource "aws_codepipeline" "test_multi_source" {
 
   provisioner "local-exec" {
     command = "aws codepipeline register-webhook-with-third-party --webhook-name ${local.webhook_name}"
+
+    environment {
+      AWS_DEFAULT_PROFILE = "${var.aws_profile}"
+      AWS_DEFAULT_REGION  = "${var.aws_region}"
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "aws codepipeline put-webhook --cli-input-json '${data.template_file.testNodeJS_webhook_json.rendered}'"
+
+    environment {
+      AWS_DEFAULT_PROFILE = "${var.aws_profile}"
+      AWS_DEFAULT_REGION  = "${var.aws_region}"
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "aws codepipeline register-webhook-with-third-party --webhook-name ${local.tnjs_webhook_name}"
 
     environment {
       AWS_DEFAULT_PROFILE = "${var.aws_profile}"
